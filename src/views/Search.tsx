8242ac1f143862e667028118d8c2cf2fc2a525af
Drag0s98 <dragostanase1698@gmail.com>
2024-10-17 13:32:26 +0200

    chore: added ui pagination

--- src/views/Search.tsx
+++ src/views/Search.tsx
@@ -11,21 +11,45 @@ export default function Search() {
       <ProductDisplay productData={searchData} />
 
       {/* Pagination */}
-      <ul className="w-full h-full flex justify-center gap-3">
-        {new Array(pages).fill("").map((_, i) => {
-          return (
-            <li
-              key={i}
+      {searchData.length > 0 && (
+        <div className="flex flex-col items-center mb-6">
+          <span className="text-sm text-gray-700 dark:text-gray-400">
+            Showing{" "}
+            <span className="font-semibold text-gray-900 dark:text-yellow-400">
+              {pagination.current.to + pagination.current.from}
+            </span>{" "}
+            to{" "}
+            <span className="font-semibold text-gray-900 dark:text-yellow-400">
+              {pages}
+            </span>{" "}
+            of{" "}
+            <span className="font-semibold text-gray-900 dark:text-yellow-400">
+              {pagination.current.total.value}
+            </span>{" "}
+            Entries
+          </span>
+          <div className="inline-flex mt-2 xs:mt-0">
+            <button
+              className="flex items-center justify-center px-3 h-8 text-sm font-medium text-yellow-400 bg-gray-800 rounded-s hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-yellow-400 dark:hover:bg-gray-700 dark:hover:text-white"
               onClick={() => {
-                pagination.current.from = 12 * i;
+                pagination.current.from = pagination.current.from - 12;
                 changePage();
               }}
             >
-              {i + 1}
-            </li>
-          );
-        })}
-      </ul>
+              Prev
+            </button>
+            <button
+              className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 border-0 border-s border-gray-700 rounded-e hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-yellow-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              onClick={() => {
+                pagination.current.from = pagination.current.from + 12;
+                changePage();
+              }}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
